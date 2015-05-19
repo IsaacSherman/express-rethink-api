959c8a16cf43a6176bb7ff9f68a6fd0ded591a8f
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2015-05-18 22:44:56 -0400

    Have example CRUD in models

--- models.js
+++ models.js
@@ -29,40 +29,49 @@ module.exports = function(Config) {
 
 			create: function(args, success) {
 				connect(function(connection) {
-					r.table('users').insert(args).run(connection, function(err, results) {
-						if (success) { success(results) }
+					r.table('users').insert(args).run(connection, function(err, result) {
+						if (err) { return console.error(err) }
+						if (success) { success(result) }
 					})
 				})
 			},
 
 			get: function(user_id, success) {
 				connect(function(connection) {
-					r.table('users').get(user_id).run(connection, function(err, results) {
-						if (success) { success(results) }
+					r.table('users').get(user_id).run(connection, function(err, result) {
+						if (err) { return console.error(err) }
+						if (success) { success(result) }
 					})
 				})
 			},
 
 			getAll: function(success) {
 				connect(function(connection) {
-					r.table('users').run(connection, function(err, results) {
-						if (success) { success(results) }
+					r.table('users').run(connection, function(err, cursor) {
+						if (err) { return console.error(err) }
+
+						cursor.toArray(function(err, result) {
+							if (err) { return console.error(err) }
+							if (success) { success(result) }
+						})
 					})
 				})
 			},
 
 			update: function(user_id, args, success) {
 				connect(function(connection) {
-					r.table('users').get(user_id).update(args).run(connection, function(err, results) {
-						if (success) { success(results) }
+					r.table('users').get(user_id).update(args).run(connection, function(err, result) {
+						if (err) { return console.error(err) }
+						if (success) { success(result) }
 					})
 				})
 			},
 
 			delete: function(user_id, success) {
 				connect(function(connection) {
-					r.table('users').get(user_id).delete().run(connection, function(err, results) {
-						if (success) { success(results) }
+					r.table('users').get(user_id).delete().run(connection, function(err, result) {
+						if (err) { return console.error(err) }
+						if (success) { success(result) }
 					})
 				})
 			}
